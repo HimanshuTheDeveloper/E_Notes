775d7e4bb97a047efeadaaaba93cc2d31c79619f
Himanshu <62435892+himanshu32301@users.noreply.github.com>
2020-06-14 14:45:41 +0530

    Add files via upload

---
+++ crud_operation.php
@@ -0,0 +1,185 @@
+<?php
+    // Connect to the database
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "crudnote";
+
+    $insert = false;
+
+    $connection = mysqli_connect($servername,$username,$password,$database);
+
+    if(!$connection)
+    {
+        die("Sorry we failed to connect :" . mysqli_connect_error());
+        
+    }
+  
+    if($_SERVER['REQUEST_METHOD']== 'POST')
+    {
+        $title = $_POST['title'];
+        $desc= $_POST['desc'];
+
+        $sql =  "INSERT INTO `notes` (`notetype`, `description`) VALUES ( '$title', '$desc')";
+
+
+        $result = mysqli_query($connection,$sql);
+
+        if($result){
+          //  echo "The record has been inserted successfully <br>";
+          $insert = true;
+
+        }
+        else{
+          echo "The record was not inserted sucessfully because of this error------> ".mysqli_error($connection);
+        }
+    
+    }
+
+
+
+
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+
+    <title>eNotes</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">eNotes</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Contactus
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<?php
+    if($insert==true){
+            echo "  <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+                    <strong>Success!</strong> Your note is sucessfully inserted.
+                      <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                        <span aria-hidden='true'>&times;</span>
+                      </button>
+                    </div>";
+                     }
+
+?>
+  
+
+
+
+    <div class="container my-4">
+    <h2>Add a Note</h2>
+    <form action="\himanshu\phptuts\crud_operation.php" method="post">
+  <div class="form-group">
+    <label for="title">Note Title</label>
+    <input type="text" class="form-control" id="title" name="title" aria-describedby="emailHelp" required>
+    <small id="emailHelp" class="form-text text-muted"></small>
+  </div>
+ 
+
+  <div class="form-group">
+    <label for="description">Description</label>
+    <textarea class="form-control" id="desc" name="desc" rows="3" required></textarea>
+  </div>
+  <button type="submit" class="btn btn-primary">Add Note</button>
+</form>
+    </div>
+
+    <div class="container my-4">
+  
+    <table class="table" id="myTable">
+  <thead>
+    <tr>
+      <th scope="col">serial number</th>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+     <?php
+    
+        $sql = "select * from notes";
+        $result = mysqli_query($connection,$sql);
+        $sno = 0;
+        while($row = mysqli_fetch_assoc($result))
+        {
+          $sno = $sno+1;         
+          echo "   <tr>
+                      <th scope='row'>".$sno."</th>
+                      <td>".  $row['notetype']."</td>
+                      <td>". $row['description']."</td>
+                      <td>Actions</td>
+                    </tr>";
+
+      
+        }
+    
+    
+    ?>
+
+ </tbody>
+</table>
+    </div>
+    <hr>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+
+    <script src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+   
+    <script>
+      $(document).ready( function () {
+    $('#myTable').DataTable();
+    } );
+    </script>
+  </body>
+</html>
+
